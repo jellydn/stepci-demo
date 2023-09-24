20e1ed8b2468d8733ad125ee238a8371425c4f3f
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2023-09-24 23:38:02 +0800

    chore: remove test code

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,15 +1,9 @@
 import fastify from 'fastify';
 import { Server, IncomingMessage, ServerResponse } from 'http';
 import { test, expect, mock } from "bun:test";
-import app from '../src/app';
-const random = mock(Math.random);
 
-test("random", async () => {
-  const val = random();
-  expect(val).toBeGreaterThan(0);
-  expect(random).toHaveBeenCalled();
-  expect(random).toHaveBeenCalledTimes(1);
-});
+// TODO: add all test cases
+import app from '../src/app';
 
 describe('Fastify API tests', () => {
   let server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse>;
