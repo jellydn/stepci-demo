0967775d4c4f91c4b9c36597eb0714cacb92b929
Huynh Duc Dung <dunghd.it@gmail.com>
2023-09-27 22:02:15 +0800

    test: define e2e test with bun test

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,37 +1,26 @@
+import { afterAll, beforeAll, describe, test, expect } from 'bun:test';
 import fastify from 'fastify';
-import assert from 'assert';
 
 import { server } from '../server';
 
-// Test for GET /api
-app.inject({
-  method: 'GET',
-  url: '/api'
-}, (err, response) => {
-  if (err) { console.error(err); assert.fail(err); }
-  assert.strictEqual(response.statusCode, 200);
-  assert.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
-});
+const app = fastify();
 
-// Test for GET /api/hello
-app.inject({
-  method: 'GET',
-  url: '/api/hello'
-}, (err, response) => {
-  if (err) { console.error(err); assert.fail(err); }
-  assert.strictEqual(response.statusCode, 200);
-  assert.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
-});
+describe('API', () => {
+  beforeAll(async () => {
+    await server.ready();
+  });
 
-// Test for GET /api/ping
-server.inject({
-  method: 'GET',
-  url: '/api/ping'
-}, (err, response) => {
-  if (err) { console.error(err); assert.fail(err); }
-  assert.strictEqual(response.statusCode, 200);
-  assert.strictEqual(response.headers['content-type'], 'application/json; charset=utf-8');
-});
+  afterAll(async () => {
+    await server.close();
+  });
 
-// The server should not be closed yet, as per user's comment
-// server.close().catch((err) => console.error(err));
+  test('should return 200', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/api',
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+})
+
